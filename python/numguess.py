d393eafe1fac6ca9c7d34ac6108befccd834861a
Ian Heafer <160186+spaceturnip@users.noreply.github.com>
2018-03-12 21:38:35 -0700

    Trying to add 1 script or snippit a day.

---
+++ python/numguess.py
@@ -0,0 +1,48 @@
+
+#!/usr/bin/env python3
+
+"""numguess.py
+
+have the user guess a number at random,
+the range can be chosen from the command line.
+"""
+
+import sys
+import random
+
+#let the range of numbers be chosen at execution time
+try:
+    numrange = int(sys.argv[1])
+except:
+    numrange = 50 #default if nothing passed in from the shell.
+
+guesses = 0
+
+theNumber = random.randrange(numrange)
+
+guessed = False
+while(not guessed):
+
+    guess = int(input("Guess a number between 1 and " + str(numrange) + " > "))
+    guesses += 1
+
+    if guess == theNumber:
+        print("That's right!\n")
+        guessed = True
+    elif guess > theNumber:
+        print("That number is too big!")
+        print("Try again.\n")
+    elif guess < theNumber:
+        print("That number is too small!")
+        print("Try again.\n")
+
+##End of while
+
+print("That took you", guesses, "guess", end='')
+
+#grammar
+if guesses == 1:
+    print(".")
+else:
+    print("es.")
+
